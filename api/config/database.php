5f90c98d24817747cff278f677a61ae3b101217a
swetarajak <swetarajak001@gmail.com>
2025-02-17 12:03:06 +0530

    datbase name error resolved

--- api/config/database.php
+++ api/config/database.php
@@ -1,5 +1,5 @@
 <?php
-class database {
+class Database {
     private $host = "localhost";
     private $db_name = "lms";
     private $username = "root";
@@ -10,7 +10,7 @@ class database {
         $this->conn = null;
         try{
             
-            $this->conn = new PDO("mysql:host=".$this->host. ";dbname".$this->db_name, $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=".$this->host. ";dbname=".$this->db_name, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         } catch (PDOException $exception){
